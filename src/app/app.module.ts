e044debfcb9071e602e187aafc7c66896311c846
rodrigoterek97 <37846417+rodrigoterek97@users.noreply.github.com>
2018-06-17 19:00:52 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { PerguntasComponent } from './perguntas/perguntas.component';
+
+const appRouts: Routes = [
+	{ path: '', component: AppComponent },
+	{ path: 'perguntas', component: PerguntasComponent }
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		PerguntasComponent
+	],
+	imports: [
+		BrowserModule,
+		AngularFireModule.initializeApp(environment.firebase),
+		AngularFireDatabaseModule,
+		FormsModule,
+		RouterModule.forRoot(
+			appRouts,
+			{ enableTracing: true }
+		)
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
